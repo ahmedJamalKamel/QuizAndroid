dd9a8d0817108f34f5b3ffdfd03c2c102a54239c
AJ <ahmad.jamal.kamel@gmail.com>
2021-12-07 02:30:41 +0200

    V2

--- app/src/main/java/com/abasathalak/mcqsapp/model/User.java
+++ app/src/main/java/com/abasathalak/mcqsapp/model/User.java
@@ -3,12 +3,29 @@ package com.abasathalak.mcqsapp.model;
 public class User {
 
     public String matchingId, cateId,  langId, authID, userID, isAvail, status, email, name, image, user_id, opponentName, opponentProfile, resut;
+    String NameUnv,NameUniL;
+    String mobile_number,Country,the_University,the_college,Squad_level;
 
 
     public User() {
     }
 
 
+    public String getNameUnv() {
+        return NameUnv;
+    }
+
+    public void setNameUnv(String nameUnv) {
+        NameUnv = nameUnv;
+    }
+
+    public String getNameUniL() {
+        return NameUniL;
+    }
+
+    public void setNameUniL(String nameUniL) {
+        NameUniL = nameUniL;
+    }
 
     public String getOpponentName() {
         return opponentName;
@@ -25,6 +42,45 @@ public class User {
     public void setOpponentProfile(String opponentProfile) {
         this.opponentProfile = opponentProfile;
     }
+    public String getMobile_number() {
+        return mobile_number;
+    }
+
+    public void setMobile_number(String mobile_number) {
+        this.mobile_number = mobile_number;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getThe_University() {
+        return the_University;
+    }
+
+    public void setThe_University(String the_University) {
+        this.the_University = the_University;
+    }
+
+    public String getThe_college() {
+        return the_college;
+    }
+
+    public void setThe_college(String the_college) {
+        this.the_college = the_college;
+    }
+
+    public String getSquad_level() {
+        return Squad_level;
+    }
+
+    public void setSquad_level(String squad_level) {
+        Squad_level = squad_level;
+    }
 
     public String getResut() {
         return resut;
@@ -33,7 +89,15 @@ public class User {
     public void setResut(String resut) {
         this.resut = resut;
     }
-
+    public User(String email, String name, String mobile_number, String country, String the_University, String the_college, String squad_level) {
+        this.email = email;
+        this.name = name;
+        this.mobile_number = mobile_number;
+        Country = country;
+        this.the_University = the_University;
+        this.the_college = the_college;
+        Squad_level = squad_level;
+    }
     public User(String first_name, String email, String user_id) {
         this.name = first_name;
         this.email = email;
